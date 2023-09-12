94718acc8c99d8da3887ee561c350475ebb4a4e1
jamie-noel <jamie-ashton@live.nmit.ac.nz>
2023-09-12 12:35:59 +1200

    dom

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 // note
 // 
 
+let btn = document.getElementById('btnCount')
+btn.addEventListener(`click`,() => { 
+    let = hTwoHeader = document.getElementsByTagName('h2')
+    alert(`the number of H2 tags are: ${hTwoHeader.length}`)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const p = document.getElementById('message');
 // console.log(p);
 
 
-let btn = document.getElementById('btnRadio');
-let output = document.getElementById('output');
+// let btn = document.getElementById('btnRadio');
+// let output = document.getElementById('output');
 
-btn.addEventListener(`click`, () => {
-    let language =document.getElementsByName('language')
-    language.forEach((language) => {
-    if(language.checked) {
-        output.innerText = `you code with : $(language.value)`
-    }
-    })
-    })
+// btn.addEventListener(`click`, () => {
+//     let language =document.getElementsByName('language')
+//     language.forEach((language) => {
+//     if(language.checked) {
+//         output.innerText = `you code with : $(language.value)`
+//     }
+//     })
+//     })
